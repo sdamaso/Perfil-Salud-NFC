0e02360c496417d0242422a479d3827bb000dbe1
Samuel Dámaso <sdamasodemiguel@gmail.com>
2023-02-09 22:30:55 +0100

    Initial Commit

---
+++ Perfil Salud NFC/Perfil_Salud_NFCApp.swift
@@ -0,0 +1,20 @@
+//
+//  Perfil_Salud_NFCApp.swift
+//  Perfil Salud NFC
+//
+//  Created by Samuel Dámaso  on 9/2/23.
+//
+
+import SwiftUI
+
+@main
+struct Perfil_Salud_NFCApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
